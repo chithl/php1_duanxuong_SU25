ebb8835b2c50f57e0e9dac707f16a5176cd00fe9
quoc toan <nguyenthimysuong99999@gmail.com>
2025-06-26 19:14:26 +0700

    Toan-David - Remove unused heart and retweet icons from product and featured item sections in index.php

--- Views/Client/index.php
+++ Views/Client/index.php
@@ -35,8 +35,6 @@
 					<div class="featured__item">
 						<div class="featured__item__pic set-bg" data-setbg="Uploads/Products/<?= $value["image"] ?? "" ?>">
 							<ul class="featured__item__pic__hover">
-								<li><a href="#"><i class="fa fa-heart"></i></a></li>
-								<li><a href="#"><i class="fa fa-retweet"></i></a></li>
 								<li>
 									<a href="index.php?page=add-to-cart&id=<?= $value["id"] ?>"><i class="fa fa-shopping-cart"></i></a>
 								</li>
